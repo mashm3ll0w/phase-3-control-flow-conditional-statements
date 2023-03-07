804b17412ee929bafd643bcb8580465ddc6d49dc
Charles Swaleh <charlswall@live.com>
2023-03-07 13:00:29 +0300

    Completed the hows_the_weather method

--- control_flow.rb
+++ control_flow.rb
@@ -9,7 +9,15 @@ def admin_login(username, password)
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
